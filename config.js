b215e6ffb56206f326961299a3cb75a68b4b7b14
changchang03 <97491274+changchang03@users.noreply.github.com>
2022-01-11 10:13:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    titleWeb: "Khảo sát",
+    introTitle: 'xin chào',
+    introDesc: `Bạn đã sẵn sàng tiến hành khảo sát?`,
+    btnIntro: 'ô cê luôn><',
+    title: 'chết rồi tui đã lỡ thích thích người ta mất rồi',
+    desc: 'liệu bạn cũng thích tui nhiều hongg?',
+    btnYes: 'Có nhaa:3',
+    btnNo: 'Không í',
+    question: 'Bạn chắc chưaaa>< Thế bạn thích tui nhiều như nào nhở?',
+    btnReply: 'Gửi cho thầu<3',
+    reply: 'siêu siêu thích',
+    mess: 'yahhh tui biết mà~. tui siêu siêu siêu thích bạn luônn😘😘',
+    messDesc: 'còn chờ gì mà hong ib cho tui:3',
+    btnAccept: 'Oki nhớ',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
